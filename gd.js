e332adbfc7ded0d7d17e24c08dbe9df51997dced
Dessiree Pasion <dessireecamillepasion65@gmail.com>
2024-11-09 19:09:03 +0800

    Add history and recipe sections for Lechon Kawali and Kare-Kare; update adobo and main dish pages

--- gd.js
+++ gd.js
@@ -19,3 +19,53 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
        }
    });
 });
+
+// MAIN DISH
+    fetch('Main Dish/mainDishPage.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('mainDish').innerHTML = data;
+    });
+
+    // adobo-section
+    fetch('Main Dish/adobo.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('adobo-section').innerHTML = data;
+    });
+
+    // sinigang-section
+    fetch('Main Dish/sinigang.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sinigang-section').innerHTML = data;
+    });
+
+    // kare-kare-section
+    fetch('Main Dish/kare.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('kareKare-section').innerHTML = data;
+    });
+
+    // tinola-section
+    fetch('Main Dish/tinola.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('tinola-section').innerHTML = data;
+    });
+
+    // lechonKawali-section
+    fetch('Main Dish/lechon.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('lechonKawali-section').innerHTML = data;
+    });
+
+    
+// DESSERT
+    fetch('Dessert/dessertPage.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('dessert').innerHTML = data;
+    });
